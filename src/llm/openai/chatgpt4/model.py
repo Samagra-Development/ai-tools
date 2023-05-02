e8c41e329b2105d3976232df2b0c2a46d3d10ef3
ChakshuGautam <chaks.gautam@gmail.com>
2023-05-02 16:57:19 +0530

    Feat: add chatgpt4

---
+++ src/llm/openai/chatgpt4/model.py
@@ -0,0 +1,30 @@
+import os
+import openai
+import openai_async
+from cache import AsyncTTL
+from .request import ModelRequest
+from tenacity import retry, wait_random_exponential, stop_after_attempt
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+class Model:
+    def __new__(cls, context):
+        cls.context = context
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(Model, cls).__new__(cls)
+        return cls.instance
+
+    @AsyncTTL(time_to_live=600000, maxsize=1024)
+    @retry(wait=wait_random_exponential(min=1, max=20), stop=stop_after_attempt(6))
+    async def inference(self, request: ModelRequest):
+        response = await openai_async.chat_complete(
+            openai.api_key,
+            timeout=20000,
+            payload={
+                "model": "gpt-4",
+                "temperature": 0,
+                "messages": request.prompt,
+            },
+        )
+        return response.json()
