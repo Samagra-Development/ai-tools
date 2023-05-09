bc514b0655f181906ae4932599dffbc90b7118ea
Gautam <102940730+GautamR-Samagra@users.noreply.github.com>
2023-05-09 11:59:50 +0530

    Update deploy.sh

--- src/text_translation/ai4bharat/deploy.sh
+++ src/text_translation/ai4bharat/deploy.sh
@@ -6,5 +6,7 @@ git clone https://github.com/pytorch/fairseq.git
 wget https://ai4b-public-nlu-nlg.objectstore.e2enetworks.net/indic2en.zip
 unzip indic2en.zip
 cd indicTrans
+pip install --ignore-installed pexpect
+pip install poetry
 poetry add gunicorn flask_cors sacremoses pandas mock sacrebleu tensorboardX pyarrow indic-nlp-library mosestokenizer subword-nmt xformers triton flask_cors webvtt-py
 poetry add ../fairseq
