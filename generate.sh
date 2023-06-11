fb71ccf67717589e6618ab5c155f8001bf6d2d01
Eesh Naugraiya <106444720+EeshNaugraiya@users.noreply.github.com>
2023-06-11 20:25:04 +0530

    refactored generate.sh file

--- generate.sh
+++ generate.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 source .env
 
 os_name=$(uname -s)
@@ -8,37 +10,31 @@ else
     sudo apt-get install jq
 fi
 
-
-count=`jq '.models | length' config.json`
+count=$(jq '.models | length' config.json)
 
 printf "version: '3'\nservices:\n" > docker-compose-generated.yaml
 
-printf "server {\n\
-    server_name ${DOMAIN_NAME};\n" > "${DOMAIN_NAME}.conf.d"
-
+printf "server {\n    server_name ${DOMAIN_NAME};\n" > "${DOMAIN_NAME}.conf.d"
 
 for ((i=0; i<$count; i++)); do
-    serviceName=`jq -r '.models['$i'].serviceName' config.json`
-    modelBasePath=`jq -r '.models['$i'].modelBasePath' config.json`
-    apiBasePath=`jq -r '.models['$i'].apiBasePath' config.json`
-    containerPort=`jq -r '.models['$i'].containerPort' config.json`
+    serviceName=$(jq -r ".models[$i].serviceName" config.json)
+    modelBasePath=$(jq -r ".models[$i].modelBasePath" config.json)
+    apiBasePath=$(jq -r ".models[$i].apiBasePath" config.json)
+    containerPort=$(jq -r ".models[$i].containerPort" config.json)
 
-    exposedPort=`expr 8000 + $i`
+    exposedPort=$((8000 + i))
 
-    environment=( $(cat config.json | jq -r '.models['$i'].environment | keys[]') )
+    environment=($(jq -r ".models[$i].environment | keys[]" config.json))
 
-    printf "    location ${apiBasePath} {\n
-            proxy_pass http://localhost:${exposedPort};\n
-        }\n" >> "${DOMAIN_NAME}.conf.d"
+    printf "    location ${apiBasePath} {\n        proxy_pass http://localhost:${exposedPort};\n    }\n" >> "${DOMAIN_NAME}.conf.d"
 
-    printf "  ${serviceName}:\n    build:\n      context: ${modelBasePath}\n    ports:\n      - ${exposedPort}:${containerPort}\n" >> docker-compose-generated.yaml
-
-    printf "    environment:\n" >> docker-compose-generated.yaml
-
+    printf "  ${serviceName}:\n    build:\n      context: ${modelBasePath}\n    ports:\n      - ${exposedPort}:${containerPort}\n    environment:\n" >> docker-compose-generated.yaml
 
     for key in "${environment[@]}"; do
-        value=`jq -r '.models['$i'].environment["'$key'"]' config.json`
+        value=$(jq -r ".models[$i].environment[\"$key\"]" config.json)
         printf "      - ${key}=${value}\n" >> docker-compose-generated.yaml
     done
 
 done
+
+echo "Script execution completed."
