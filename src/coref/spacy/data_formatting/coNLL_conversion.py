6701bc8aa83ff699239c594dfb4362c6e6f6436f
J <jiya.cm.20@nitj.ac.in>
2023-06-15 19:03:39 +0530

    CoNLL conversion done, required modules spacy_conll and spacy_stanza

--- src/coref/spacy/data_formatting/coNLL_conversion.py
+++ src/coref/spacy/data_formatting/coNLL_conversion.py
@@ -1,1 +1,12 @@
 from spacy_conll import init_parser
+
+nlp = init_parser("en",
+                  "stanza",
+                  parser_opts={"use_gpu": True, "verbose": False},
+                  include_headers=True)
+
+doc = nlp("A cookie is a baked or cooked food that is typically small, flat and sweet. It usually contains flour, sugar and some type of oil or fat.")
+
+# Get the CoNLL representation of the whole document, including headers
+conll = doc._.conll_str
+print(conll)
