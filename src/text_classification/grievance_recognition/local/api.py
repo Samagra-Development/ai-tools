430c518d492d5520d36aa15119b30dbf64ce18d6
Tushar Gupta <tushar@samagragovernance.in>
2023-07-25 05:45:12 +0000

    feat: Make models use GPU when available and pre-create a model in startup instead of handler.

--- src/text_classification/grievance_recognition/local/api.py
+++ src/text_classification/grievance_recognition/local/api.py
@@ -5,13 +5,20 @@ import aiohttp
 
 app = Quart(__name__)
 
+model = None
+
 @app.before_serving
 async def startup():
     app.client = aiohttp.ClientSession()
+    global model
+    model = Model(app)
 
 @app.route('/', methods=['POST'])
 async def embed():
+    global model
     data = await request.get_json()
     req = ModelRequest(**data)
-    model = Model(app)
     return await model.inference(req)
+
+if __name__ == "__main__":
+    app.run()
