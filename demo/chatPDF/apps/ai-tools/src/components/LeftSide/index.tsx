d7ce776c99866ef7833b6fcd811849fa5c8accac
prtkjakhar <prtkjakhar@gmail.com>
2023-07-27 16:10:39 +0530

    Added pdf pages navigation

--- demo/chatPDF/apps/ai-tools/src/components/LeftSide/index.tsx
+++ demo/chatPDF/apps/ai-tools/src/components/LeftSide/index.tsx
@@ -10,32 +10,56 @@ const LeftSide = () => {
   const context = useContext(AppContext);
   const { pdfList, setPdfList, selectedPdf, setSelectedPdf } = context;
 
-  const onDrop = (acceptedFiles: File[]) => {
+  const onDrop = async (acceptedFiles: File[]) => {
     const updatedPdfList = acceptedFiles.map((file) => ({
       file,
       preview: URL.createObjectURL(file),
     }));
     setPdfList([...pdfList, ...updatedPdfList]);
     setSelectedPdf(updatedPdfList[updatedPdfList.length - 1]); // set the last uploaded file as selected
+
+    // Send each file to the API
+    for (const file of acceptedFiles) {
+      const formData = new FormData();
+      formData.append('file', file);
+      
+      // Modify the URL and headers according to your API
+      // const response = await axios.post('/api/your-endpoint', formData, {
+      //   headers: {
+      //     'Content-Type': 'multipart/form-data',
+      //   },
+      // });
+
+      // // Handle the response here
+      // console.log(response.data);
+    }
   };
 
   // Method to select a PDF
   const selectPdf = (pdf: any) => {
+    console.log("hie", pdf)
     // Revoke the URL of the currently-selected PDF, if there is one
-  if (selectedPdf) {
-    URL.revokeObjectURL(selectedPdf.preview);
-  }
+    if (selectedPdf) {
+      URL.revokeObjectURL(selectedPdf.preview);
+    }
 
-  // Create a new object URL for the selected PDF
-  const newPreview = URL.createObjectURL(pdf.file);
+    // Create a new object URL for the selected PDF
+    const newPreview = URL.createObjectURL(pdf.file);
 
-   // Update the preview for the selected PDF and set it as selected
-   const newPdf = { ...pdf, preview: newPreview };
-   setSelectedPdf(newPdf);
+    // Update the preview for the selected PDF and set it as selected
+    const newPdf = { ...pdf, preview: newPreview };
+    setSelectedPdf(newPdf);
 
     // Update the preview in the pdfList
-  const newPdfList = pdfList.map(p => p.file.name === pdf.file.name ? newPdf : p);
-  setPdfList(newPdfList);
+    const newPdfList = pdfList.map((p: any) =>
+      p.file.name === pdf.file.name ? newPdf : p
+    );
+    setPdfList(newPdfList);
+
+
+    // Read pdf text
+    
+
   };
 
   return (
@@ -56,7 +80,10 @@ const LeftSide = () => {
 
       <div className={styles.pdflist}>
         {pdfList.map((pdf: any, i: number) => (
-          <div className={styles.pdfElement} key={i} onClick={() => selectPdf(pdf)}>
+          <div
+            className={styles.pdfElement}
+            key={i}
+            onClick={() => selectPdf(pdf)}>
             <div className={styles.imageContainer}>
               <Image src={messageIcon} alt="" layout="responsive" />
             </div>
--- demo/chatPDF/apps/ai-tools/src/components/MiddleSide/index.tsx
+++ demo/chatPDF/apps/ai-tools/src/components/MiddleSide/index.tsx
@@ -1,26 +1,62 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useState } from 'react';
 import styles from './index.module.css';
+import nextIcon from '../../assets/icons/nextIcon.svg';
+import prevIcon from '../../assets/icons/previousIcon.svg';
+import Image from 'next/image';
 import { AppContext } from '../../context';
 import { Document, Page, pdfjs } from 'react-pdf';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-
 const MiddleSide = () => {
   const context = useContext(AppContext);
+  const [totalPages, setTotalPages] = useState(0);
+  const [pageNumber, setPageNumber] = useState(1);
   const { selectedPdf } = context;
   console.log('hie', selectedPdf);
+
+  function onDocumentLoadSuccess({ numPages }: { numPages: number }) {
+    setTotalPages(numPages);
+    URL.revokeObjectURL(selectedPdf.preview);
+  }
+
+  function handleNext() {
+    if (pageNumber < totalPages) {
+      setPageNumber(pageNumber + 1);
+    }
+  }
+  function handlePrevious() {
+    if (pageNumber > 0) {
+      setPageNumber(pageNumber - 1);
+    }
+  }
 
   return (
     <div className={styles.main}>
       {selectedPdf && selectedPdf.preview && (
-        <div key={selectedPdf.file.name}>
-          <Document
-            file={selectedPdf.preview}
-            onLoadSuccess={() => URL.revokeObjectURL(selectedPdf.preview)}
-          >
-            <Page pageNumber={1} />
-          </Document>
-        </div>
+        <>
+          <div key={selectedPdf.file.name}>
+            <Document
+              file={selectedPdf.preview}
+              onLoadSuccess={onDocumentLoadSuccess}>
+              <Page pageNumber={pageNumber} />
+            </Document>
+            <div className={styles.footer}>
+              <div className={styles.pageText}>
+                Page {pageNumber} of {totalPages}
+              </div>
+              <div className={styles.buttonContainer}>
+                <button onClick={handlePrevious} disabled={pageNumber === 1}>
+                <Image src={prevIcon} alt="" width={25} height={25}/>
+                </button>
+                <button
+                  onClick={handleNext}
+                  disabled={pageNumber === totalPages}>
+                  <Image src={nextIcon} alt="" width={25} height={25}/>
+                </button>
+              </div>
+            </div>
+          </div>
+        </>
       )}
     </div>
   );
