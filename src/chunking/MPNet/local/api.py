519cda796f50464e46ae0599d99ceafb9c2c83d6
Karun Agarwal <113603846+singhalkarun@users.noreply.github.com>
2023-07-31 11:43:26 +0530

    Added text file upload (Gautam)

--- src/chunking/MPNet/local/api.py
+++ src/chunking/MPNet/local/api.py
@@ -1,11 +1,13 @@
 from model import Model
 from request import ModelRequest
 from quart import Quart, request, Response, send_file  # <- Don't forget to import send_file
+
+quart.config['QUART_SERVE_STATIC_TIMEOUT'] = 600
 import aiohttp
 import pandas as pd
 import io
 
-app = Quart(__name__)
+app = Quart(_name_)
 
 model = None
 
@@ -14,13 +16,24 @@ async def startup():
     app.client = aiohttp.ClientSession()
     global model
     model = Model(app)
+    
 
 @app.route('/', methods=['POST'])
 async def embed():
     global model
     data = await request.get_json()
-    req = ModelRequest(**data)
-    response = await model.inference(req)  # Await the coroutine to get the actual response
+    files = await request.files  # await the coroutine
+    uploaded_file = files.get('file')  # now you can use .get()
+
+    if uploaded_file:
+        print("1- File uploaded")
+        text_data = uploaded_file.stream.read().decode('utf-8')
+        req = ModelRequest(text = text_data)  # Pass the DataFrame to ModelRequest
+        response = await model.inference(req)
+
+    else : 
+        req = ModelRequest(**data)
+        response = await model.inference(req)  # Await the coroutine to get the actual response
 
     df = pd.read_csv(io.StringIO(response))  # Convert the CSV string back to a DataFrame
 
