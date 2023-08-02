56ea77cbb9a12a1d83270015730bbf0a1d904059
Karun Agarwal <113603846+singhalkarun@users.noreply.github.com>
2023-08-02 14:23:26 +0530

    Update generate_independent_docker.sh

--- generate_independent_docker.sh
+++ generate_independent_docker.sh
@@ -33,11 +33,9 @@ for ((i=0; i<$count; i++)); do
 
     # Add environment variables to docker-compose.yaml
     if [[ ${#environment[@]} -gt 0 ]]; then
-        printf "    environment:\n" >> docker-compose-independent-generated.yaml
+        printf "    environment:\n" >> docker-compose-generated.yaml
     fi
     for key in "${environment[@]}"; do
-        value_name=`jq -r '.models['$i'].environment["'$key'"]' config.json`
-        eval "value=$value_name"
-        printf "      - ${key}=${value}\n" >> docker-compose-independent-generated.yaml
+        printf "      - ${key}= \${${key}}\n" >> docker-compose-generated.yaml
     done
 done
