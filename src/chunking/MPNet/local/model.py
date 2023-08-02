c98f559d4114a3ea8a50b275f495735fb949cc36
GautamR-Samagra <gautam@samagragovernance.in>
2023-08-02 07:58:13 +0000

    embed_chunk_page_number

--- src/chunking/MPNet/local/model.py
+++ src/chunking/MPNet/local/model.py
@@ -15,45 +15,53 @@ import nltk
 from quart import Response
 
 nltk.download('punkt')
-
 class Splitter:
-    def __init__(self,doc,max_chunk_length):
-        self.doc = doc
+    def __init__(self, pages, max_chunk_length):
+        self.pages = pages
         self.sentencesInDoc = []
         self.max_chunk_length = max_chunk_length
         self.embedding = None
         self.docToSentences()
 
     def docToSentences(self):
-        sentences = sent_tokenize(self.doc)
-        for sentence in sentences:
-            currSentenceSplits = textwrap.wrap(sentence.strip(), self.max_chunk_length)
-            self.sentencesInDoc.extend(currSentenceSplits)
+        for page_num, page_content in self.pages:
+            sentences = sent_tokenize(page_content)
+            for sentence in sentences:
+                currSentenceSplits = textwrap.wrap(sentence.strip(), self.max_chunk_length)
+                for split in currSentenceSplits:
+                    self.sentencesInDoc.append((split, page_num))
+
 
     def getChunksConsideringSentences(self):
-      ## chunk having one sentence only. Keep on adding sentences using docToSentences until you reach maximum words threshold
         chunks = []
         curr_sentence = ''
-        for sentence in self.sentencesInDoc:
+        start_page = end_page = None
+        for sentence, page_num in self.sentencesInDoc:
             if len(curr_sentence + sentence) > self.max_chunk_length:
-                chunks.append(curr_sentence)
+                chunks.append((curr_sentence, start_page, end_page))
                 curr_sentence = sentence
+                start_page = end_page = page_num
             else:
                 curr_sentence = curr_sentence + ' ' + sentence
-        chunks.append(curr_sentence)
+                end_page = page_num
+                if start_page is None:
+                    start_page = page_num
+        chunks.append((curr_sentence, start_page, end_page))
         return chunks
 
+
     # taken from https://medium.com/@npolovinkin/how-to-chunk-text-into-paragraphs-using-python-8ae66be38ea6
-    def getChunksConsideringNeighbouringSimilarity(self,numberOfSentencesToCalculatedWeightedSum=10):
+    def getChunksConsideringNeighbouringSimilarity(self, numberOfSentencesToCalculatedWeightedSum=10):
         encoding_model = SentenceTransformer('all-mpnet-base-v2')
 
         if self.embedding is None:
             print('Document Encoding Process :-')
+            sentences_only = [sentence for sentence, _ in self.sentencesInDoc]
             self.embedding = encoding_model.encode(
-                    self.sentencesInDoc,
+                    sentences_only,
                     show_progress_bar=True,
                     batch_size=32,
-                    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+                    device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
                 )
 
         similarities = cosine_similarity(self.embedding)
@@ -61,17 +69,38 @@ class Splitter:
         minimas = argrelextrema(activated_similarities, np.less, order=2)[0].reshape(-1)
         return self.getChunksWithMinimas(minimas)
 
-    def getChunksWithMinimas(self,minimas):
+
+    def getChunksWithMinimas(self, minimas):
         chunks = []
         chunk_start = 0
+        chunk_text = ''
+        start_page = None
+        end_page = None
+
         for minima in minimas:
-            mergedSentence = ' '.join(self.sentencesInDoc[chunk_start:minima])
+            for i in range(chunk_start, minima):
+                sentence, page_num = self.sentencesInDoc[i]
+                chunk_text += ' ' + sentence
+                end_page = page_num
+                if start_page is None:
+                    start_page = page_num
+
+            chunks.append((chunk_text.strip(), start_page, end_page))
+            chunk_text = ''
+            start_page = end_page = None
             chunk_start = minima
-            currSentenceSplits = textwrap.wrap(mergedSentence, self.max_chunk_length)
-            chunks.extend(currSentenceSplits)
-        if(chunk_start != len(self.sentencesInDoc) - 1):
-            chunks.extend(self.sentencesInDoc[chunk_start:])
+
+        if chunk_start != len(self.sentencesInDoc):
+            for i in range(chunk_start, len(self.sentencesInDoc)):
+                sentence, page_num = self.sentencesInDoc[i]
+                chunk_text += ' ' + sentence
+                end_page = page_num
+                if start_page is None:
+                    start_page = page_num
+            chunks.append((chunk_text.strip(), start_page, end_page))
+
         return chunks
+
 
 
     def rev_sigmoid(self,x:float)->float:
@@ -120,10 +149,13 @@ class Model():
 
     async def inference(self, request: ModelRequest):
     # Modify this function according to model requirements such that inputs and output remains the same
-        splitter = Splitter(request.text, 4 * 1024)
+        pages =  request.text
+        splitter = Splitter(pages, 4 * 1024)
         chunks = splitter.getChunksConsideringNeighbouringSimilarity()
-        df = pd.DataFrame({'content': chunks})
-        # Convert DataFrame to a CSV string
+        df = pd.DataFrame({'content': [content for content, _, _ in chunks],
+                        'start_page': [start_page for _, start_page, _ in chunks],
+                        'end_page': [end_page for _, _, end_page in chunks]})
+
         csv_string = df.to_csv(index=False)
 
         # Properly escape the CSV string
