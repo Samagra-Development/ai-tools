502b2ffa1ded588b4bdf5f3966b12f5f24995612
Amruth-Vamshi <130033232+Amruth-Vamshi@users.noreply.github.com>
2023-11-07 11:04:27 +0530

    update GPT4 model

--- src/llm/openai/chatgpt4/model.py
+++ src/llm/openai/chatgpt4/model.py
@@ -22,7 +22,7 @@ class Model:
             openai.api_key,
             timeout=20000,
             payload={
-                "model": "gpt-4",
+                "model": "gpt-4-1106-preview",
                 "temperature": 0,
                 "messages": [{"role":"user","content" : request.prompt}],
             },
