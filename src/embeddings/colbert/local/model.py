9a5d162d48adf3dca7f92ad5635131cce89b159a
Gautam <102940730+GautamR-Samagra@users.noreply.github.com>
2024-03-19 16:17:18 +0530

    Update model.py

--- src/embeddings/colbert/local/model.py
+++ src/embeddings/colbert/local/model.py
@@ -13,24 +13,6 @@ class Model():
         if not hasattr(cls, 'instance'):
             cls.instance = super(Model, cls).__new__(cls)
         # Initialize Colbert
-        cls.df = pd.read_csv('/Testing1.csv')
-        cls.df['PID'] = cls.df.index.astype(str)
-        with Run().context(RunConfig(experiment='notebook')):
-            cls.searcher = Searcher(index='import pandas as pd
-from ragatouille import RAGPretrainedModel
-from request import ModelRequest
-from colbert import Indexer, Searcher
-from colbert.infra import Run, RunConfig, ColBERTConfig
-from colbert.data import Queries, Collection
-
-
-
-class Model():
-    def __new__(cls, context):
-        cls.context = context
-        if not hasattr(cls, 'instance'):
-            cls.instance = super(Model, cls).__new__(cls)
-        # Initialize Colbert
         cls.df = pd.read_csv('Testing1.csv')
         cls.df['PID'] = cls.df.index.astype(str)
         with Run().context(RunConfig(experiment='notebook')):
@@ -41,20 +23,7 @@ class Model():
 
     async def inference(self, request: ModelRequest):
         query = request.text
-        k = request.k  
-        column_returned = 'id'  
-        results = self.searcher.search(query, k)
-        searched_ids = self.df.loc[results[0], column_returned].to_list()
-        searched_content = self.df.loc[results[0], 'content'].to_list()
-        return {"ids": searched_ids, "content": searched_content, "scores": results[2]}
-/', collection=cls.df['content'].to_list())
-        print(cls.df.columns)
-        
-        return cls.instance
-
-    async def inference(self, request: ModelRequest):
-        query = request.text
-        k = request.k  
+        k = int(request.k )
         column_returned = 'id'  
         results = self.searcher.search(query, k)
         searched_ids = self.df.loc[results[0], column_returned].to_list()
